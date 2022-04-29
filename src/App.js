9ea6a54b12e4d013b34e5439b3e2291ce2c87ae2
Abhijeet <abhijeetjawle01@gmail.com>
2022-04-29 20:04:59 +0530

    Added link to subdomain

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 function App() {
   return (
+  
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <h1>Hello from V2</h1><p>
+        <h1>Hello from V2</h1>
+        <button><a href='https://app.horizonai.tech/'> To App</a></button>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
           
